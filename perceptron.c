8de8f2b1239dd4adae86e781b6f58ee07aa82628
thales-g5s <thalesvitor2001@gmail.com>
2019-11-24 03:50:28 -0300

    definido corretamente os tipos de variaveis e pesos do perceptron

--- perceptron.c
+++ perceptron.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#define threshold 7
-#define peso1 4 
-#define peso2 2 
-#define peso3 3
+#define threshold 5
+#define peso1 3 
+#define peso2 1 
+#define peso3 2
 
 int main () {
-		int x1, x2, x3;
-		int w1 = peso1, w2 = peso2, w3 = peso3;
-		short int resposta;
-		int soma;
+	int x1, x2, x3;
+	float w1 = peso1, w2 = peso2, w3 = peso3;
+	float soma;
+	short int resposta;
 
-		scanf("%d %d %d", &x1, &x2, &x3);
+	scanf("%d %d %d", &x1, &x2, &x3);
 
-		soma = x1 * w1 + x2 * w2 + x3 * w3;
+	soma = x1 * w1 + x2 * w2 + x3 * w3;
 
-		if (soma >= threshold) resposta= 1;
-		else reaposta = 0;
+	if (soma >= threshold) resposta= 1;
+	else resposta = 0;
 
-		printf("> %d\n", resposta);
-		return 0;
+	printf("> %d\n", resposta);
+	return 0;
 }
