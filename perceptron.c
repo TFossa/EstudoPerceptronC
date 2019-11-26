4e1d487ec60eb65578146d394c03980b44bfaf4e
Thales Fossalussa <thalesvitor2001@gmail.com>
2019-11-26 18:51:19 -0300

    Implementando terceiro perceptron

--- perceptron.c
+++ perceptron.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #define thresholdp1 1 
 #define thresholdp2 0 
-#define peso 1 
+#define thresholdp3 0 
+#define pesop1p2 1 
+#define pesor1p3 1
+#define pesor2p3 -1
 
 int main () {
 	int x1, x2;
-	float w1 = peso, w2 = peso;
-	float soma;
-	short int resposta1, resposta2, respostaf;
+	float w1 = pesop1p2, w2 = pesop1p2;
+	float w3 = pesor1p3, w4 = pesor2p3;
+	float somap1, somap2, somap3;
+	short int respostap1, respostap2, respostap3;
 
 	scanf("%d %d", &x1, &x2);
 
 	// Primeiro perceptron > and
-	soma = x1 * w1 + x2 * w2;
+	somap1 = x1 * w1 + x2 * w2;
 
-	if (soma <= thresholdp1) resposta1 = 0;
-	else if (soma > thresholdp1) resposta1 = 1;
+	if (somap1 <= thresholdp1) respostap1 = 0;
+	else if (somap1 > thresholdp1) respostap1 = 1;
 
 	// segundo perceptron > or
+	somap2 = x1 * w1 + x2 * w2;
 
-	if (soma <= thresholdp2) resposta2 = 0;
-	else if (soma > thresholdp2) resposta2 = 1;
+	if (somap2 <= thresholdp2) respostap2 = 0;
+	else if (somap2 > thresholdp2) respostap2 = 1;
 
 	// perceptron final
-	if (resposta1 == resposta2) respostaf = 0;
-	else respostaf = 1;
+	somap3 = respostap1 * w3 + respostap2 * w4;
 	
+	if (somap3 == thresholdp3) respostap3 = 0;
+	else respostap3 = 1;
 
-	printf("> %d\n", respostaf);
+	printf("> %d\n", respostap3);
 	return 0;
 }
