0bd1c9a0ffb7b77f3e386a5d668ddde9dc2b68dc
Thales Fossalussa <thalesvitor2001@gmail.com>
2019-11-26 15:28:32 -0300

    Perceptron simulando XOR

--- perceptron.c
+++ perceptron.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
-#define threshold -1 
-#define peso1 1 
-#define peso2 -2
+#define thresholdp1 1 
+#define thresholdp2 0 
+#define peso 1 
 
 int main () {
 	int x1, x2;
-	float w1 = peso1, w2 = peso2;
+	float w1 = peso, w2 = peso;
 	float soma;
-	short int resposta;
+	short int resposta1, resposta2, respostaf;
 
 	scanf("%d %d", &x1, &x2);
 
+	// Primeiro perceptron > and
 	soma = x1 * w1 + x2 * w2;
 
-	if (soma <= threshold) resposta = 0;
-	else if (soma > threshold) resposta = 1;
+	if (soma <= thresholdp1) resposta1 = 0;
+	else if (soma > thresholdp1) resposta1 = 1;
 
-	printf("> %d\n", resposta);
+	// segundo perceptron > or
+
+	if (soma <= thresholdp2) resposta2 = 0;
+	else if (soma > thresholdp2) resposta2 = 1;
+
+	// perceptron final
+	if (resposta1 == resposta2) respostaf = 0;
+	else respostaf = 1;
+	
+
+	printf("> %d\n", respostaf);
 	return 0;
 }
